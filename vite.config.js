619890e2d8788e03a06ffd8d794aede2955b5be6
yachanliu <yachanliu@umass.edu>
2025-09-29 20:13:32 -0400

    build

--- vite.config.js
+++ vite.config.js
@@ -2,3 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { viteSingleFile } from 'vite-plugin-singlefile'
 export default defineConfig({ plugins: [react(), viteSingleFile()] })
+export default defineConfig({
+  base: "zeolite-classifier",
+});
